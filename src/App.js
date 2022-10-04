6b80a33ca07a6c9510efe70633cc27cbba2da72e
ndtrang121 <ndtrang129@gmail.com>
2022-10-04 08:54:34 +0700

    Auto format code with Prettier

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import { Fragment } from 'react';
+import { Fragment } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import DefaultLayout from './layouts/DefaultLayout'
-import NotFound from './pages/NotFound';
-import Login from './pages/Login';
-import Start from './pages/Start';
+import NotFound from './pages/NotFound'
+import Login from './pages/Login'
+import Start from './pages/Start'
 import { loginRoutes } from './routes'
 import { LogoutRequire, LoginRequire } from './components/Auth'
 
 function App() {
-	return (
-		<Routes>
-			<Route element={<LoginRequire />} >
-				{loginRoutes.map((route, index) => {
-					let Layout = DefaultLayout
-					if (route.layout) {
-						Layout = route.layout
-					} else if (route.layout === null) {
-						Layout = Fragment
-					}
+    return (
+        <Routes>
+            <Route element={<LoginRequire />}>
+                {loginRoutes.map((route, index) => {
+                    let Layout = DefaultLayout
+                    if (route.layout) {
+                        Layout = route.layout
+                    } else if (route.layout === null) {
+                        Layout = Fragment
+                    }
 
-					const Page = route.component
-					return (
-						<Route
-							key={index}
-							path={route.path}
-							element={
-								<Layout>
-									<Page />
-								</Layout>
-							}
-						/>
-					)
-				})
-				}
-			</Route>
-			<Route element={<LogoutRequire />} >
-				<Route path="/" element={<Start />} />
-				<Route path="/login" element={<Login />} />
-			</Route>
-			<Route
-				path='*'
-				element={<NotFound />}
-			/>
-		</Routes>
-	);
+                    const Page = route.component
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    )
+                })}
+            </Route>
+            <Route element={<LogoutRequire />}>
+                <Route path="/" element={<Start />} />
+                <Route path="/login" element={<Login />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    )
 }
 
-export default App;
+export default App
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,53 +1,50 @@
-import classNames from "classnames/bind";
+import classNames from 'classnames/bind'
 import { Link } from 'react-router-dom'
 import styles from './Button.module.scss'
 
 const cx = classNames.bind(styles)
 
 function Button({
-	to,
-	href,
-	children,
-	faq = false,
-	primary = false,
-	disabled = false,
-	leftIcon,
-	rightIcon,
-	onClick,
-	className,
-	rotateIcon,
-	...passProps
+    to,
+    href,
+    children,
+    faq = false,
+    primary = false,
+    disabled = false,
+    leftIcon,
+    rightIcon,
+    onClick,
+    className,
+    rotateIcon,
+    ...passProps
 }) {
+    let Comp = 'button'
+    const props = {
+        onClick,
+        ...passProps,
+    }
 
+    if (to) {
+        props.to = to
+        Comp = Link
+    } else if (href) {
+        props.href = href
+        Comp = 'a'
+    }
 
-	let Comp = 'button'
-	const props = {
-		onClick,
-		...passProps
-	}
+    const classes = cx('wrapper', {
+        primary,
+        faq,
+        [className]: className,
+    })
 
-
-	if (to) {
-		props.to = to
-		Comp = Link
-	} else if (href) {
-		props.href = href
-		Comp = 'a'
-	}
-
-	const classes = cx('wrapper', {
-		primary,
-		faq,
-		[className]: className
-	})
-
-	return (
-		<Comp className={classes} {...props}>
-			{leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
-			{children}
-			{rightIcon && <span className={cx('icon', { [rotateIcon]: rotateIcon })}>{rightIcon}</span>}
-		</Comp>
-	);
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            {children}
+            {rightIcon && <span className={cx('icon', { [rotateIcon]: rotateIcon })}>{rightIcon}</span>}
+        </Comp>
+    )
 }
 
-export default Button;
+export default Button
