bf115920f881ea769242d8e1389401acc61d0b4c
ndtrang121 <ndtrang129@gmail.com>
2022-12-06 11:44:27 +0700

    [update]: modified NotFound page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,15 +1,30 @@
-import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind'
+import { Link } from 'react-router-dom'
+import styles from './NotFound.module.scss'
+
+const cx = classNames.bind(styles)
 
 const NotFound = () => (
-	<div className="not-found">
-		<img
-			src="https://www.pngitem.com/pimgs/m/561-5616833_image-not-found-png-not-found-404-png.png"
-			alt="not-found"
-		/>
-		<Link to="/browse" className="link-home">
-			Go Home
-		</Link>
-	</div>
-);
+    <div className={cx('not-found')}>
+        <h1 className={cx('title')}>Lost your way?</h1>
+        <p className={cx('zoom-area')}>Sorry, we can't find that page. You'll find lots to explore on the home page.</p>
+        <section className={cx('error-container')}>
+            <span className={cx('four')}>
+                <span className={cx('screen-reader-text')}>4</span>
+            </span>
+            <span className={cx('zero')}>
+                <span className={cx('screen-reader-text')}>0</span>
+            </span>
+            <span className={cx('four')}>
+                <span className={cx('screen-reader-text')}>4</span>
+            </span>
+        </section>
+        <div className={cx('link-container')}>
+            <Link to={'/browse'} className={cx('more-link')}>
+                Netflix Home
+            </Link>
+        </div>
+    </div>
+)
 
-export default NotFound;
+export default NotFound
